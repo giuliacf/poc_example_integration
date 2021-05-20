5e19ccaa9b4d0d39a6481f93e57992eb89408ceb
Giulia de Carvalho <giuliacarvalhof@gmail.com>
2021-05-20 20:14:02 -0300

    create splash screen

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:poc_example_integration/app/modules/splash/splash_store.dart';
+import 'package:poc_example_integration/utils/colors.dart';
 
 class SplashPage extends StatefulWidget {
   final String title;
@@ -12,18 +14,31 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends ModularState<SplashPage, SplashStore> {
+
+  initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1000));
+    Modular.to.navigate('/login');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          store.increment();
-        },
-        child: Icon(Icons.add),
-      ),
-    );
+        backgroundColor: lead,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/logo.png"),
+              SizedBox(height: 30),
+              CircularProgressIndicator.adaptive(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  aqua,
+                ),
+                strokeWidth: 2,
+              ),
+            ],
+          ),
+        ));
   }
 }
